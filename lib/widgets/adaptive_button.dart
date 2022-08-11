257d58934fbe8bfe173d0b33f9abb2ab20e4f2e0
Benny <ebere.benny97@gmail.com>
2022-08-11 15:51:16 +0100

    first commit

---
+++ lib/widgets/adaptive_button.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveButton extends StatelessWidget {
+  final String text;
+  final VoidCallback handler;
+
+  AdaptiveButton(this.text, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          )
+        : TextButton(
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          );
+  }
+}
---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,74 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionItem extends StatefulWidget {
+  const TransactionItem({
+    Key? key,
+    required this.transaction,
+    required this.deleteTx,
+  }) : super(key: key);
+
+  final Transaction transaction;
+  final Function deleteTx;
+
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color? _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple,
+    ];
+    _bgColor = availableColors[Random().nextInt(4)];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: _bgColor,
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FittedBox(child: Text('\$${widget.transaction.amount}')),
+          ),
+        ),
+        title: Text(
+          widget.transaction.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(widget.transaction.date),
+        ),
+        trailing: MediaQuery.of(context).size.width > 460
+            ? TextButton.icon(
+                style:
+                    TextButton.styleFrom(primary: Theme.of(context).errorColor),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
+                icon: const Icon(Icons.delete),
+                label: const Text('Delete'),
+              )
+            : IconButton(
+                onPressed: () => widget.deleteTx(widget.transaction.id),
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+              ),
+      ),
+    );
+  }
+}
